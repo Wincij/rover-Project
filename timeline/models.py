bcddeaab5013dd44e8c1796f3508b42ed3f1a37c
Wincij <m.pajaczkowski95@gmail.com>
2018-11-08 23:26:34 +0100

    Postgresql works

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=120)
+    pub_date = models.DateTimeField(null=False)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/', max_length = 100)
