c8bdc257988a073af13ea5ede84fa6ec6bddd398
Wincij <m.pajaczkowski95@gmail.com>
2018-11-23 19:24:50 +0100

    Ready do push (settings.py now not included) - passwords reseting

--- supporters/models.py
+++ supporters/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
+
+
 class Supporter(models.Model):
     name = models.CharField(max_length = 200)
     image = models.ImageField(upload_to='images/supporters')
     website = models.CharField(max_length = 200)
 
 
-# For readable admin panel and managing 
+# For readable admin panel and managing
     def __str__(self):
         return self.name
